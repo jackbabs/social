93ff6209eb6bbcdfdac7977e34cbeb2e5e9c4ef9
jackbabs <thejackbabs@gmail.com>
2018-11-24 15:17:37 +0000

    server

---
+++ server.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+
+const app = express();
+
+app.use(express.static('dist'))
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
+
+// DB Config 
+const db = process.env.DATABASE_URL
+
+mongoose.connect(db).then(() => console.log('MongoDB Connected')).catch(err => console.log(err));
+
+// Passport Config 
+// require('./config/passport')(passport);
+
+// Use Routes
+app.get('/', (req, res) => res.json({ msg: "claudia this is a coded message to tell you we must depart the barbican and find a sandwich"}))
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Server running on port ${port}`))
+
+
+
+
+
+
